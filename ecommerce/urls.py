0292f1b10abb6281cd6f98f1eb36ea3129a09676
Oresanya.Ayoola <oreayoola1@gmail.com>
2025-09-01 23:46:35 +0100

    Added home route and cleaned API structure for deployment

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -3,7 +3,8 @@ from django.urls import path, include
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Welcome to the E-commerce API")
+    return HttpResponse("Welcome to the E-commerce API. Use /api/ for endpoints.")
+
 urlpatterns = [
      path('', home),
     path('admin/', admin.site.urls),
